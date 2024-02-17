03ed44f56cd5f43683a002eaa5634e602c50ebe2
vedjaiswal <vedjaiswal0506@gmail.com>
2024-02-17 23:21:43 +0530

    conflict

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
-import Legal from "./Pages/Legal";
+import Legal from './Pages/Legal';
 import NotFound from "./Pages/NotFound";
 import Appointment from "./Pages/Appointment";
 import Login from "./Components/login/Login";
